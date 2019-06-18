373f3d51ca28b8a83df616b335126818c80a5330
olatundesleek <47743182+olatundesleek@users.noreply.github.com>
2019-06-18 14:02:23 +0100

    Update tip.js
    removed window location

--- tip.js
+++ tip.js
@@ -5,5 +5,4 @@ var bill =document.getElementById("bill").value;
 var tip =document.getElementById("tipamount").value; 
 var total = bill * tip;
 alert('Hello '+ name +', you left N'+ total +' tip for '+ waiter);
-  window.location.href = "index2.html";
 }
