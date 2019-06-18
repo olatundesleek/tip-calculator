0a4f65264cfbe05000620b4deab29c567898065a
olatundesleek <47743182+olatundesleek@users.noreply.github.com>
2019-06-18 13:31:39 +0100

    Update tip.js
    added windows location to alert

--- tip.js
+++ tip.js
@@ -4,5 +4,6 @@ var waiter =document.getElementById("waitress").value;
 var bill =document.getElementById("bill").value; 
 var tip =document.getElementById("tipamount").value; 
 var total = bill * tip;
-alert('Hello '+ name +', you left a tip of N'+ total +' for '+ waiter);
+alert('Hello '+ name +', you left N'+ total +' tip for '+ waiter);
+  window.location.href = "index2.html";
 }
